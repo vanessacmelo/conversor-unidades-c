66d87329bb31f213052995c0f13d2bf434ac7f92
Pedro Henrique Santos Souza Duarte <142743646+peudev10@users.noreply.github.com>
2024-12-17 20:35:02 -0300

    Programa Conversor de Unidades em Linguagem C

---
+++ conversor_unidades_c/main.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <locale.h>
+
+void Metro_Centrimetro_Milimetro();
+
+int main() {
+    setlocale(LC_ALL, ""); 
+    int opcao = 0;
+
+    printf("\n\n------------ Conversor de unidades em C ------------\n");
+    printf("[1] Conversor de metro, centímetro e milímetro\n");
+    printf("------------------------------------------------------\n\n");
+
+    printf("Escolha a opção: ");
+    scanf("%d", &opcao); // escolhendo a opção
+
+    switch(opcao){
+        case 1: 
+            Metro_Centrimetro_Milimetro();
+            break;
+
+        default:
+            printf("opção inválida!");
+            break;
+    }
+    return 0;
+}
+
+// funções do menu
+void Metro_Centrimetro_Milimetro(){
+    int opcao = 0;
+    float grandeza = 0;
+
+    printf("\n\n Indique a grandeza do valor que você vai inserir\n");
+    printf("[1] Metro\n");
+    printf("[2] Centímetro\n");
+    printf("[3] Milímetro\n");
+
+    printf("Escolha a opção: ");
+    scanf("%d", &opcao); // escolhendo a opção
+
+    printf("Digite a garandeza: ");
+    scanf("%f", &grandeza); // escolhendo a opção
+
+    switch(opcao){
+        case 1: 
+            printf("\nValor digitado %.2f em metros\n", grandeza);
+            printf("Valor em centímetros: %.2f cm\n", grandeza * 100);
+            printf("Valor em milímetros: %.2f mm\n", grandeza * 1000);
+            break;
+        case 2: 
+            printf("\nValor digitado %.2f em Centímetro\n", grandeza);
+            printf("Valor em metros: %.2f m\n", grandeza / 100);
+            printf("Valor em milímetros: %.2f mm\n", grandeza * 10);
+            break;
+        case 3: 
+            printf("\nValor digitado %.2f em Milímetro\n", grandeza);
+            printf("Valor em metros: %.2f m\n", grandeza / 1000);
+            printf("Valor em centímetros: %.2f cm\n", grandeza / 10);
+            break;
+
+        default:
+            printf("opção inválida!");
+            break;
+    }
+}
